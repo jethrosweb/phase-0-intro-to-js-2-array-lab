bce64278fb165733361e31458838f7062da6817d
Jethro Dean <jethroldean@gmail.com>
2022-02-22 17:02:47 +1100

    commit tree 3f8d406e5e76dd09a76e185d990c2328387e0f90

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 // Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
+let cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name) {
-    cats.push(name)
+function destructivelyAppendCat(name) { 
+    cats.push(name);
 }
 
 function destructivelyPrependCat(name) {
-    cats.unshift(name)
+    cats.unshift(name);
 }
 
 function destructivelyRemoveLastCat() {
-    cats.pop()
+    cats.pop();
 }
 
 function destructivelyRemoveFirstCat() {
-    cats.shift()
+    cats.shift();
 }
 
 function appendCat(name) {
-    return [...cats, name]
+    return [...cats, name];
 }
 
 function prependCat(name) {
-    return [name, ...cats]
+    return [name, ...cats];
 }
 
 function removeLastCat() {
-    return cats.slice(0, -1)
+    return cats.slice(0, -1);
 }
 
 function removeFirstCat() {
-    return cats.slice(1)
+    return cats.slice(1);
 }
